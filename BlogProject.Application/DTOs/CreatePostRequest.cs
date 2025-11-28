9ec433d932554920dfc7f8d5f297fe2e3d0a899b
CansuuUrgun <olcansu96@gmail.com>
2025-11-28 13:26:54 +0300

    Add project files.

---
+++ BlogProject.Application/DTOs/CreatePostRequest.cs
@@ -0,0 +1,1 @@
+﻿public record CreatePostRequest(string Title, string Content);
---
+++ BlogProject.Domain/Entities/Post.cs
@@ -0,0 +1,7 @@
+﻿public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
